572b8a7156a0c620fe4e7a735a436571ccb3dd1d
Chuddington <6558350+Chuddington@users.noreply.github.com>
2024-12-09 09:24:40 +0000

    Add `Map` object to `PaymentFetcher` constructor
    - Use the `Map` as a backing data structure for the PaymentFetcher.
      By doing so, the fetcher's testability increases as we can control
      this at the time of object instantiation.
    
    Kata: UnusualSpending

--- src/main/java/com/github/chuddington/tddkata/spending/PaymentFetcher.java
+++ src/main/java/com/github/chuddington/tddkata/spending/PaymentFetcher.java
@@ -1,12 +1,20 @@
 package com.github.chuddington.tddkata.spending;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Obtains {@link Payments} for use in calculation / activity monitoring.
  */
 public class PaymentFetcher {
+
+    private final Map<Integer, Payments> backingData;
+
+    public PaymentFetcher(Map<Integer, Payments> backingData) {
+        this.backingData = backingData;
+    }
+
     public Payments fetch(int userId) {
-        return Payments.of(new ArrayList<>());
+        return this.backingData.getOrDefault(userId, Payments.of());
     }
 }
--- src/test/java/com/github/chuddington/tddkata/spending/PaymentFetcherTest.java
+++ src/test/java/com/github/chuddington/tddkata/spending/PaymentFetcherTest.java
@@ -2,17 +2,41 @@ package com.github.chuddington.tddkata.spending;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class PaymentFetcherTest {
 
-    private final PaymentFetcher fetcher = new PaymentFetcher();
+    private final Payments emptyPayments = Payments.of();
+    private final Map<Integer, Payments> backingData = Map.ofEntries(
+            Map.entry(1, Payments.of(new Payment()))
+    );
+    private final PaymentFetcher fetcher = new PaymentFetcher(backingData);
 
     @Test
-    void thereAreNoPaymentsWithoutAnId() {
+    void obtainsEmptyPaymentsWithAnInvalidId() {
         assertEquals(
-                Payments.of(),
+                emptyPayments,
                 fetcher.fetch(0)
+        );
+        assertEquals(
+                emptyPayments,
+                fetcher.fetch(Integer.MIN_VALUE)
+        );
+        assertEquals(
+                emptyPayments,
+                fetcher.fetch(Integer.MAX_VALUE)
+        );
+    }
+
+    @Test
+    void obtainsPaymentsDataViaTheConstructor() {
+        assertNotEquals(
+                fetcher.fetch(0),
+                fetcher.fetch(1)
         );
     }
 }
